88feeefbeb803acccdaab159bb097419d1c3ec13
Jim Campbell <jimcampbell355@gmail.com>
2023-12-14 21:12:54 -0500

    initial commit

---
+++ IO.Library/IOHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace JCLibraries.IO.Library
+{
+    public static class IOHandler
+    {
+        static string ReadText(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string? text = sr.ReadToEnd();
+                return text;
+            }
+        }
+        static void WriteText(string decryptedText, string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.Write(decryptedText);
+            }
+        }
+    }
+}
