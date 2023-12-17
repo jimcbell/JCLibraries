e89223c87a183d915663e6faf8ae1ed34bb9e068
Jim Campbell <jimcampbell355@gmail.com>
2023-12-17 18:39:38 -0500

    Library updates for odata extensions

---
+++ OData.Json.Helper/ODataExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace OData.Json.Helper;
+
+public static class ODataExtensions
+{
+    private static readonly string _value = "value";
+    public static async Task<T?> ParseContent<T>(this HttpResponseMessage response)
+    {
+        JsonElement jsonElement = await response.Content.ReadFromJsonAsync<JsonElement>();
+        jsonElement.TryGetProperty(_value, out JsonElement content);
+        T? deserializedObject = JsonSerializer.Deserialize<T>(content);
+        if (deserializedObject is null)
+        {
+            throw new Exception("Deserialized object is null");
+        }
+        return deserializedObject;
+    }
+}
